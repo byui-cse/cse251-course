ed2a6d2a30e4de3c74d2a5bb063515fbe9f452dd
Luc Comeau <comeaul@byui.edu>
2021-01-12 14:58:05 -0700

    Added a call to print_dict()

--- week02/team/team_get_deck_id.py
+++ week02/team/team_get_deck_id.py
@@ -15,12 +15,22 @@ Instructions:
 import requests
 import json
 
+# Include cse 251 common Python files
+import os, sys
+sys.path.append('../../code')
+from cse251 import *
+
 if __name__ == '__main__':
 
     response = requests.get(r'https://deckofcardsapi.com/api/deck/new/')
     # Check the status code to see if the request succeeded.
     if response.status_code == 200:
         data = response.json()
+
+        # Function is from the cse251 common code 
+        print_dict(data)
+
+        
         if data['success'] == True:
             print(data['deck_id'])
         else:
