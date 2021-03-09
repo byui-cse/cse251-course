b7c1517b9a532dd9289f097a4adc12c831d18ed2
Luc Comeau <comeaul@byui.edu>
2021-03-09 09:05:50 -0700

    Changed assignment 10

--- week10/assignment/assignment.py
+++ week10/assignment/assignment.py
@@ -10,7 +10,7 @@ Instructions:
 
 - Review TODO comments
 - writer: a process that will "write"/send numbers to the reader.  
-  To keep things simple, have the writer send consecutive numbers starting at 1.
+  To keep things simple, send random values from 0 to 255 to the reader.
 - reader: a process that receive numbers sent by the writer.
 - You don't need any sleep() statements for either process.
 - You are able to use lock(s) and semaphores(s).
